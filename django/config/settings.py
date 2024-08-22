b07211e9d8ac6813cb8c1dcce508c04d0a070360
mazimi surface <mohammad.azimi.1996@gmail.com>
2024-08-22 13:07:06 +0330

    celery settings added

--- django/config/settings.py
+++ django/config/settings.py
@@ -147,3 +147,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.renderers.XMLRenderer',
     ]
 }
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'amqp://guest:guest@localhost//')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_SECRET_KEY = os.getenv("CELERY_SECRET_KEY", "DefaultSecretKey")
